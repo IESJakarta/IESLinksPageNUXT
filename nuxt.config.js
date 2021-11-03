2f531a63abd19bfef10617c42e081e3c5a0a48a2
msrody <mike.rody@gmail.com>
2021-11-03 08:47:44 +0700

    fixed top text wrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'IESLinksPageNUXT',
+    title: 'IES Jakarta Quick Links',
     htmlAttrs: {
       lang: 'en'
     },
@@ -14,7 +14,21 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
       { name: 'msapplication-TileColor', content: '#da532c' },
-      { name: 'theme-color', content: '#ffffff' }
+      { name: 'theme-color', content: '#ffffff' },
+      { name: 'description', content: 'We are a church committed to serve the English-speaking community in Jakarta.' },
+      { property: 'og:title', content: 'IES Jakarta' },
+      { property: 'og:description',
+              content: 'We are a church committed to serve the English-speaking community in Jakarta.' },
+      { property: 'og:url', content: 'https://iesjak.art' },
+      { property: 'og:image', content: 'https://iesjak.art/graphics/IES-logoWhiteBack.png' },
+      { property: 'og:image:secure_url', content: 'https://iesjak.art/graphics/IES-logoWhiteBack.png' },
+      { property: 'og:updated_time', content: '1633093082000' },
+      { property: 'profile:username', content: 'IESJakarta' },
+      { name: 'twitter:title', content: 'IES Jakarta' },
+      { name: 'twitter:description',
+              content: 'We are a church committed to serve the English-speaking community in Jakarta.' },
+      { name: 'twitter:image', content: 'https://iesjak.art/graphics/IES-logoWhiteBack.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
     ],
     link: [
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon_package_v0.16/apple-touch-icon.png' },
